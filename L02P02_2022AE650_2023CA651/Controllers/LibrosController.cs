e0aea4c9bb8b2a0492083b34c273cb6d0558830a
miche <miche@DESKTOP-EEADCHD>
2025-04-03 19:48:53 -0600

    Correcciones en tabla libros y creacion del controlador libros con su listado

---
+++ L02P02_2022AE650_2023CA651/Controllers/LibrosController.cs
@@ -0,0 +1,22 @@
+﻿using L02P02_2022AE650_2023CA651.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022AE650_2023CA651.Controllers
+{
+
+    public class LibrosController : Controller
+    {
+        private readonly libreriaDbContext _context;
+
+        public LibrosController(libreriaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Prototipo02(int id)
+        {
+            var libros = _context.libros.Where(l => l.id_autor == id).ToList();
+
+            return View(libros);
+        }
+    }
+}
