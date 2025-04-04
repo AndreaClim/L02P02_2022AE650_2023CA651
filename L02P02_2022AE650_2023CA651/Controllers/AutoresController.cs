6861ee68a0dca3461a950562f6bf73fe997690e3
miche <miche@DESKTOP-EEADCHD>
2025-04-03 19:29:08 -0600

    prototipo 01 creado con listado de autores

---
+++ L02P02_2022AE650_2023CA651/Controllers/AutoresController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using L02P02_2022AE650_2023CA651.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022AE650_2023CA651.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private readonly libreriaDbContext _context;
+
+        public AutoresController(libreriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Prototipo01()
+        {
+            var listaAutores = _context.autores.ToList();
+            return View(listaAutores);
+        }
+
+
+    }
+}
