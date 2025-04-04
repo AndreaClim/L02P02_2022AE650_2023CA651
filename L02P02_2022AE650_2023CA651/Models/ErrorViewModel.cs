f30b658cbbc5b87508d20aa6ac8c780115a0224c
miche <miche@DESKTOP-EEADCHD>
2025-04-03 18:09:53 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022AE650_2023CA651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022AE650_2023CA651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
