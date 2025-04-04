e230b98baac931f892b8dadc6b7dd125eaa49988
Amaya-2003 <caluchogamer@gmail.com>
2025-04-03 18:49:05 -0600

    DbContext

--- L02P02_2022AE650_2023CA651/Models/libreriaDbContext.cs
+++ L02P02_2022AE650_2023CA651/Models/libreriaDbContext.cs
@@ -8,9 +8,12 @@ public class libreriaDbContext : DbContext
     public libreriaDbContext(DbContextOptions<libreriaDbContext> options) : base(options)
     {
     }
-    public DbSet<marcas> marcas { get; set; }
-    public DbSet<equipos> equipos { get; set; }
-    public DbSet<usuarios> usuarios { get; set; }
-
+    public DbSet<clientes> clientes { get; set; }
+    public DbSet<autores> autores { get; set; }
+    public DbSet<categorias> categorias { get; set; }
+    public DbSet<libros> libros { get; set; }
+    public DbSet<pedido_encabezado> pedido_encabezado { get; set; }
+    public DbSet<pedido_detalle> pedido_detalle { get; set; }
+    public DbSet<comentarios_libros> comentarios_libros { get; set; }
 }
 
