783b8f246b3fbff279397496b76220006d719e0a
miche <miche@DESKTOP-EEADCHD>
2025-04-03 18:45:57 -0600

    agregacion de clases/tablas

---
+++ L02P02_2022AE650_2023CA651/Models/comentarios_libros.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace L02P02_2022AE650_2023CA651.Models
+{
+    public class comentarios_libros
+    {
+        [Key]
+        public int id { get; set; }
+        public int? id_libro { get; set; }
+        public string? comentarios { get; set; }
+        public string? usuario { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
