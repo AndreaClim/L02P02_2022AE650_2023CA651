3ba223222bbf5e73ecbfc9977a574414e449ab83
Amaya-2003 <caluchogamer@gmail.com>
2025-04-03 18:35:16 -0600

    Program.cs con builder conexion a BD y Primera version de dbcontext

---
+++ L02P02_2022AE650_2023CA651/Models/libreriaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace L02P02_2022AE650_2023CA651.Models;
+
+public class libreriaDbContext : DbContext
+{
+    public libreriaDbContext(DbContextOptions<libreriaDbContext> options) : base(options)
+    {
+    }
+    public DbSet<marcas> marcas { get; set; }
+    public DbSet<equipos> equipos { get; set; }
+    public DbSet<usuarios> usuarios { get; set; }
+
+}
+
--- L02P02_2022AE650_2023CA651/Program.cs
+++ L02P02_2022AE650_2023CA651/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<libreriaDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("libreriaDbConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
